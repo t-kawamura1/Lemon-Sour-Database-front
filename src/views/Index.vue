ebb330d1b12a9a653528e5f1f5c7a6d9782d737e
tkawamura <tkawamura.stayhome@gmail.com>
2021-06-02 15:23:33 +0900

    add: axios for api

---
+++ src/views/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.get('http://localhost:3000/api/v1/hello')
+        .then(res => {
+          this.msgs.push(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
